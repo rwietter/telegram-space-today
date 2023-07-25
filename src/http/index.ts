06ad4f2335e59199fbbe5e5314c873217e7f9a01
rwietter <rwietter@zohomail.com>
2023-07-25 12:01:33 -0300

    remove protocol

--- src/http/index.ts
+++ src/http/index.ts
@@ -7,28 +7,26 @@ const api = axios.create({
   baseURL: `https://api.nasa.gov`
 });
 
-// const TelegrafSingleton = (function () {
-//   let instance: Telegraf<Context> | null = null;
+const TelegrafSingleton = (function () {
+  let instance: Telegraf<Context> | null = null;
 
-//   function createInstance() {
-//     const bot = new Telegraf(BOT_TOKEN);
+  function createInstance() {
+    const bot = new Telegraf(BOT_TOKEN);
 
-//     return bot;
-//   }
+    return bot;
+  }
 
-//   return {
-//     getInstance() {
-//       if (!instance) {
-//         instance = createInstance();
-//       }
+  return {
+    getInstance() {
+      if (!instance) {
+        instance = createInstance();
+      }
 
-//       return instance;
-//     },
-//   };
-// })();
+      return instance;
+    },
+  };
+})();
 
-// const bot = TelegrafSingleton.getInstance();
-
-const bot = new Telegraf('6612128919:AAHK8c7uUeG_XilTDklp4Xv8BPfXf5jUxtM')
+const bot = TelegrafSingleton.getInstance();
 
 export { bot, Context, api };
